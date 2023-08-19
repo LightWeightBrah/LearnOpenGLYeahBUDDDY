a37059bba2d9d6118a61f5ccb57e2e9e54815b53
SeprAY <julian.labanowski@wp.pl>
2023-08-19 10:07:26 +0200

    Shader Class

--- LearnOpenGLYeahBUDDDY/Main.cpp
+++ LearnOpenGLYeahBUDDDY/Main.cpp
@@ -1,21 +1,22 @@
-#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include "Shader.h"
+
+#include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 float vertices[] = {
-	 0.5f,  0.5f, 0.0f, //top right
-	 0.5f, -0.5f, 0.0f, //bottom right
-	-0.5f, -0.5f, 0.0f, //bottom left
-	-0.5f,  0.5f, 0.0f, //top left
-
+	//posiitions			//colors
+	 0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,
+	-0.5f, -0.5f, 0.0f,	  0.0f, 1.0f, 0.0f,	
+	 0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f
 };
 
 unsigned int indices[] = {
 	0, 1, 2,
-	2, 3, 0
 };
 
 int main()
@@ -41,12 +42,9 @@ int main()
 		std::cout << "Failed to initlaize GLAD" << std::endl;
 		return -1;
 	}
-
 	glViewport(0, 0, 800, 600);
 
-	int nrAttributes;
-	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+	Shader shader("Basic.vert", "Basic.frag");
 
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
@@ -57,86 +55,21 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	const char* vertexShaderSource = 
-		"#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"out vec4 vertexColor;\n"//sending vertexColor to frag shader
-		"void main()\n"
-		"{\n"
-		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
-		"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
-		"}\0";
-
-	const char* fragmentShaderSource = 
-		"#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"in vec4 vertexColor;"//receiving vertexColor input from vertex
-		"void main()\n"
-		"{\n"
-		"	FragColor = vertexColor;\n"
-		"}\0";
-
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	//ERROR CHECKING
-	int  success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-	//ERROR CHECKING
-
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	//ERROR CHECKING
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-	//ERROR CHECKING
-
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	//ERROR CHECKING
-	glGetProgramiv(shaderProgram, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-	//ERROR CHECKING
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
 	//dont unbind element buffer before unbinding vertex array
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-	// uncomment this call to draw in wireframe polygons.
+	//uncomment this call to draw in wireframe polygons.
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	while (!glfwWindowShouldClose(window))
@@ -146,9 +79,8 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
+		shader.Use();
 		glBindVertexArray(VAO);
-
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
@@ -158,7 +90,7 @@ int main()
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
-	glDeleteProgram(shaderProgram);
+	shader.Delete();
 
 	glfwTerminate();
 	return 0;
